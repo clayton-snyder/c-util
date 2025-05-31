aad303d03a42f2f81f4f2796ceaa3769180ffa20
Clayton <snyderc@gmx.com>
2025-05-31 15:46:13 -0700

    Add stringutils module

---
+++ stringutils.c
@@ -0,0 +1,8 @@
+#include "stringutils.h"
+
+bool strutils_startswith(const char* str, const char* prefix) {
+    assert(str != NULL);
+    assert(prefix != NULL);
+    return strncmp(str, prefix, strlen(prefix)) == 0;
+}
+
---
+++ stringutils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdbool.h>
+#include <string.h>
+
+bool strutils_startswith(const char* str, const char* prefix);
+

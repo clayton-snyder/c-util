f54d1f1750ddd0748fffa9c88220b71432ad776f
Clayton <snyderc@gmx.com>
2025-06-10 15:00:37 -0700

    Add write-to-buffer versions of all ANSI escape color/format functions.

--- terminalutils.c
+++ terminalutils.c
@@ -3,6 +3,230 @@
 #include <assert.h>
 #include <stdio.h>
 
+// Character to begin an ANSI escape sequence
+#define ESC "\033"
+
+// ESC"[38;5;255m". Last value is 0-255, and the rest is static. 
+#define ESC_SEQ_COLOR_256_MAX_LEN 11
+
+// color_str refers to a hex string or a color name. The longest color names are
+// "yellow" and "purple", shorter than "#FFFFFF".
+#define MAX_COLOR_STR_LEN 7
+
+// ESC"]12;#FFFFFF"ESC. 
+#define ESC_SEQ_CURSOR_COLOR_MAX_LEN (6 + MAX_COLOR_STR_LEN)
+
+// Writes 'str' to 'buf' if its length is less than 'max'. Does not write the
+// null terminator or consider it in the size. Returns true if the string fits 
+// and was written.
+static bool write_if_fits(char *const buf, rsize_t max, const char *const str) {
+    size_t len = strlen(str);
+    if (len > max) return false;
+
+    for (size_t i = 0; i < len; i++)
+        buf[i] = str[i];
+
+    return true;
+}
+
+bool termutils_set_bold_buf(char *const buf, rsize_t maxlen, bool on) {
+    return write_if_fits(buf, maxlen, on ? ESC"[1m" : ESC"[22m");
+}
+
+bool termutils_set_faint_buf(char *const buf, rsize_t maxlen, bool on) {
+    return write_if_fits(buf, maxlen, on ? ESC"[2m" : ESC"[22m");
+}
+
+bool termutils_set_italic_buf(char *const buf, rsize_t maxlen, bool on) {
+    return write_if_fits(buf, maxlen, on ? "\033[3m" : "\033[23m");
+}
+
+bool termutils_set_underline_buf(char *const buf, rsize_t maxlen, bool on) {
+    return write_if_fits(buf, maxlen, on ? "\033[4m" : "\033[24m");
+}
+
+bool termutils_set_blinking_buf(char *const buf, rsize_t maxlen, bool on) {
+    return write_if_fits(buf, maxlen, on ? "\033[5m" : "\033[25m");
+}
+
+bool termutils_set_inverse_buf(char *const buf, rsize_t maxlen, bool on) {
+    return write_if_fits(buf, maxlen, on ? "\033[7m" : "\033[27m");
+}
+
+bool termutils_set_hidden_buf(char *const buf, rsize_t maxlen, bool on) {
+    return write_if_fits(buf, maxlen, on ? "\033[8m" : "\033[28m");
+}
+
+bool termutils_set_strikethrough_buf(char *const buf, rsize_t maxlen, bool on) {
+    return write_if_fits(buf, maxlen, on ? "\033[9m" : "\033[29m");
+}
+
+bool termutils_set_text_color_buf(
+        char *const buf, rsize_t maxlen, termutils_color color)
+{
+    switch (color) {
+    case TERMUTILS_COLOR_BLACK:
+        return write_if_fits(buf, maxlen, "\033[30m");
+    case TERMUTILS_COLOR_RED:
+        return write_if_fits(buf, maxlen, "\033[31m");
+    case TERMUTILS_COLOR_GREEN:
+        return write_if_fits(buf, maxlen, "\033[32m");
+    case TERMUTILS_COLOR_YELLOW:
+        return write_if_fits(buf, maxlen, "\033[33m");
+    case TERMUTILS_COLOR_BLUE:
+        return write_if_fits(buf, maxlen, "\033[34m");
+    case TERMUTILS_COLOR_MAGENTA:
+        return write_if_fits(buf, maxlen, "\033[35m");
+    case TERMUTILS_COLOR_CYAN:
+        return write_if_fits(buf, maxlen, "\033[36m\0");
+    case TERMUTILS_COLOR_WHITE:
+        return write_if_fits(buf, maxlen, "\033[37m");
+    case TERMUTILS_COLOR_BLACK_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[90m");
+    case TERMUTILS_COLOR_RED_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[91m");
+    case TERMUTILS_COLOR_GREEN_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[92m");
+    case TERMUTILS_COLOR_YELLOW_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[93m");
+    case TERMUTILS_COLOR_BLUE_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[94m");
+    case TERMUTILS_COLOR_MAGENTA_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[95m");
+    case TERMUTILS_COLOR_CYAN_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[96m");
+    case TERMUTILS_COLOR_WHITE_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[97m");
+    case TERMUTILS_COLOR_DEFAULT:
+        return write_if_fits(buf, maxlen, "\033[39m");
+    default:
+#ifdef TERMUTILS_DEBUG_ASSERT
+        assert(!"[set_text_color()] Invalid TERMUTILS_COLOR");
+#endif
+        return false;
+    }
+}
+
+bool termutils_set_text_color_256_buf(
+        char *const buf, rsize_t maxlen, int color_code)
+{
+#ifdef TERMUTILS_DEBUG_ASSERT
+    assert(color_code >= 0 && color_code <= 255);
+    // don't call this if you don't have room
+    assert(maxlen >= ESC_SEQ_COLOR_256_MAX_LEN);
+#endif
+    if (maxlen < ESC_SEQ_COLOR_256_MAX_LEN) return false;
+
+    char seq[ESC_SEQ_COLOR_256_MAX_LEN + 1];
+    int len = sprintf_s(seq, sizeof(seq), ESC"[38;5;%dm", color_code);
+
+#ifdef TERMUTILS_DEBUG_ASSERT
+    assert(len <= ESC_SEQ_COLOR_256_MAX_LEN);
+#endif
+    if (len > (int) maxlen) return false;
+    return write_if_fits(buf, len, seq);
+}
+
+bool termutils_reset_text_color_buf(char *const buf, rsize_t maxlen) {
+    return write_if_fits(buf, maxlen, "\033[39m");
+}
+
+bool termutils_set_bg_color_buf(
+        char *const buf, rsize_t maxlen, termutils_color color)
+{
+    switch (color) {
+    case TERMUTILS_COLOR_BLACK:
+        return write_if_fits(buf, maxlen, "\033[40m");
+    case TERMUTILS_COLOR_RED:
+        return write_if_fits(buf, maxlen, "\033[41m");
+    case TERMUTILS_COLOR_GREEN:
+        return write_if_fits(buf, maxlen, "\033[42m");
+    case TERMUTILS_COLOR_YELLOW:
+        return write_if_fits(buf, maxlen, "\033[43m");
+    case TERMUTILS_COLOR_BLUE:
+        return write_if_fits(buf, maxlen, "\033[44m");
+    case TERMUTILS_COLOR_MAGENTA:
+        return write_if_fits(buf, maxlen, "\033[45m");
+    case TERMUTILS_COLOR_CYAN:
+        return write_if_fits(buf, maxlen, "\033[46m");
+    case TERMUTILS_COLOR_WHITE:
+        return write_if_fits(buf, maxlen, "\033[47m");
+    case TERMUTILS_COLOR_BLACK_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[100m");
+    case TERMUTILS_COLOR_RED_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[101m");
+    case TERMUTILS_COLOR_GREEN_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[102m");
+    case TERMUTILS_COLOR_YELLOW_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[103m");
+    case TERMUTILS_COLOR_BLUE_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[104m");
+    case TERMUTILS_COLOR_MAGENTA_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[105m");
+    case TERMUTILS_COLOR_CYAN_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[106m");
+    case TERMUTILS_COLOR_WHITE_BRIGHT:
+        return write_if_fits(buf, maxlen, "\033[107m");
+    case TERMUTILS_COLOR_DEFAULT:
+        return write_if_fits(buf, maxlen, "\033[49m");
+    default:
+#ifdef TERMUTILS_DEBUG_ASSERT
+        assert(!"[set_bg_color()] Invalid TERMUTILS_COLOR");
+#endif
+        return false;
+    }
+}
+
+bool termutils_set_bg_color_256_buf(
+        char *const buf, rsize_t maxlen, int color_code)
+{
+#ifdef TERMUTILS_DEBUG_ASSERT
+    assert(color_code >= 0 && color_code <= 255);
+    // don't call this if you don't have room
+    assert(maxlen >= ESC_SEQ_COLOR_256_MAX_LEN);
+#endif
+    if (maxlen < ESC_SEQ_COLOR_256_MAX_LEN) return false;
+
+    char seq[ESC_SEQ_COLOR_256_MAX_LEN + 1];
+    int len = sprintf_s(seq, sizeof(seq), ESC"[48;5;%dm", color_code);
+
+#ifdef TERMUTILS_DEBUG_ASSERT
+    assert(len <= ESC_SEQ_COLOR_256_MAX_LEN);
+#endif
+    return write_if_fits(buf, len, seq);
+}
+
+bool termutils_reset_bg_color_buf(char *const buf, rsize_t maxlen) {
+    return write_if_fits(buf, maxlen, "\033[49m");
+}
+
+bool termutils_reset_all_buf(char *const buf, rsize_t maxlen) {
+    return write_if_fits(buf, maxlen, "\033[0m");
+}
+
+bool termutils_set_cursor_color_buf(
+        char *const buf, rsize_t maxlen, const char *const color_str)
+{
+#ifdef TERMUTILS_DEBUG_ASSERT
+    assert(strlen(color_str) < MAX_COLOR_STR_LEN);
+#endif
+    if (strlen(color_str) > MAX_COLOR_STR_LEN) return false;
+
+#ifdef TERMUTILS_DEBUG_ASSERT
+    assert(maxlen >= ESC_SEQ_CURSOR_COLOR_MAX_LEN);
+#endif
+    if (maxlen < ESC_SEQ_CURSOR_COLOR_MAX_LEN) return false;
+
+    char seq[ESC_SEQ_CURSOR_COLOR_MAX_LEN];
+    sprintf_s(seq, sizeof(seq), "\x1b]12;%s\x07", color_str);
+
+    return write_if_fits(buf, maxlen, seq);
+}
+
+bool termutils_reset_cursor_color_buf(char *const buf, rsize_t maxlen) {
+    return write_if_fits(buf, maxlen, "\x1b]112\x07");
+}
+
 void termutils_set_bold(bool on) {
     fputs(on ? "\033[1m" : "\033[22m", stdout);
 }
--- terminalutils.h
+++ terminalutils.h
@@ -15,10 +15,20 @@
 // To enable debug asserts, define the TERMUTILS_DEBUG_ASSERT macro. This will
 // assert validity of the color code and enum values passed in. Otherwise, if
 // invalid values are passed, the function will quietly abort.
+//
+//      PRINT vs. WRITE TO BUFFER
+// Functions with the "_buf" suffix take a char* and a max length and write the
+// ANSI escape sequence into the char*, returning false if the sequence could 
+// not fit in the provided space. Functions without the suffix simply print the
+// format string to stdout. The set of functions is identical for both. Clearly,
+// using the "_buf" functions will generally be more efficient, assuming you
+// plan to write multiple escape sequences into the buffer between each print.
+// Use the buffer pattern when doing heavy formatting.
 
 #pragma once
 
 #include <stdbool.h>
+#include <string.h>
 
 typedef enum {
     TERMUTILS_COLOR_BLACK,
@@ -39,6 +49,52 @@ typedef enum {
     TERMUTILS_COLOR_WHITE_BRIGHT,
     TERMUTILS_COLOR_DEFAULT
 } termutils_color;
+
+bool termutils_set_bold_buf(char *const buf, rsize_t maxlen, bool on);
+bool termutils_set_faint_buf(char *const buf, rsize_t maxlen, bool on);
+bool termutils_set_italic_buf(char *const buf, rsize_t maxlen, bool on);
+bool termutils_set_underline_buf(char *const buf, rsize_t maxlen, bool on);
+bool termutils_set_blinking_buf(char *const buf, rsize_t maxlen, bool on);
+bool termutils_set_inverse_buf(char *const buf, rsize_t maxlen, bool on);
+bool termutils_set_hidden_buf(char *const buf, rsize_t maxlen, bool on);
+bool termutils_set_strikethrough_buf(
+        char *const buf, rsize_t maxlen, bool on);
+
+// Sets the text (foreground) color using the 8-16 color set (some terminals may
+// not support the _BRIGHT versions of colors).
+bool termutils_set_text_color_buf(
+        char *const buf, rsize_t maxlen, termutils_color color);
+
+// Sets the text (foreground) color using the ANSI 256 color table code.
+bool termutils_set_text_color_256_buf(
+        char *const buf, rsize_t maxlen, int color_code);
+
+// Same as calling `set_text_color(TERMUTILS_COLOR_DEFAULT);`
+bool termutils_reset_text_color_buf(char *const buf, rsize_t maxlen);
+
+// Sets the background color using the 8-16 color set (some terminals may not
+// support the _BRIGHT versions of colors).
+bool termutils_set_bg_color_buf(
+        char *const buf, rsize_t maxlen, termutils_color color);
+
+// Sets the background color using the ANSI 256 color table code.
+bool termutils_set_bg_color_256_buf(
+        char *const buf, rsize_t maxlen, int color_code);
+
+// Same as calling `set_bg_color(TERMUTILS_COLOR_DEFAULT);`
+bool termutils_reset_bg_color_buf(char *const buf, rsize_t maxlen);
+
+// Resets all colors and formatting options to their defaults.
+bool termutils_reset_all_buf(char *const buf, rsize_t maxlen);
+
+// color_str can be a hex string or a color name. 
+bool termutils_set_cursor_color_buf(
+        char *const buf, rsize_t maxlen, const char *const color_str);
+
+// I have noticed this not working in some terminals (e.g., WezTerm). OSC 112
+// should reset the cursor color to default, but I'm not sure how default gets
+// set in the first place. It works on Windows Terminal.
+bool termutils_reset_cursor_color_buf(char *const buf, rsize_t maxlen);
 
 void termutils_set_bold(bool on);
 void termutils_set_faint(bool on);

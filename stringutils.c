bfd6bc698be07cb9a6a00342d8df5d16cb069860
Clayton <snyderc@gmx.com>
2025-06-22 23:34:28 -0700

    Stringutils: add strut_ctoi to convert a character in the range '0' - '9' to the corresponding integer. Add case-insensitive versions of strcmp, strncmp, and startswith

--- stringutils.c
+++ stringutils.c
@@ -1,6 +1,8 @@
 #include "stringutils.h"
 
 #include <assert.h>
+#include <ctype.h>
+#include <string.h>
 
 bool strut_startswith(const char* str, const char* prefix) {
     assert(str != NULL);
@@ -8,3 +10,31 @@ bool strut_startswith(const char* str, const char* prefix) {
     return strncmp(str, prefix, strlen(prefix)) == 0;
 }
 
+bool strut_startswithi(const char* str, const char* prefix) {
+    assert(str != NULL);
+    assert(prefix != NULL);
+    return strut_strncmpi(str, prefix, strlen(prefix)) == 0;
+}
+
+int strut_strncmpi(const char *const a, const char *const b, size_t count) {
+#ifdef _WIN32
+    return _strnicmp(a, b, count);
+#else
+    return strncasecmp(a, b, count);
+#endif
+}
+
+int strut_strcmpi(const char *const a, const char *const b) {
+#ifdef _WIN32
+    return _stricmp(a, b);
+#else
+    return strcasecmp(a, b);
+#endif
+
+}
+
+int strut_ctoi(char c) {
+    int result = c - '0';
+    if (result < 0 || result > 9) return -1;
+    return result;
+}
--- stringutils.h
+++ stringutils.h
@@ -4,4 +4,11 @@
 #include <string.h>
 
 bool strut_startswith(const char* str, const char* prefix);
+bool strut_startswithi(const char* str, const char* prefix);
+
+int strut_strncmpi(const char *const a, const char *const b, size_t count);
+int strut_strcmpi(const char *const a, const char *const b);
+
+// Returns -1 if the character is out of the range of '0' - '9'
+int strut_ctoi(char c);
 
